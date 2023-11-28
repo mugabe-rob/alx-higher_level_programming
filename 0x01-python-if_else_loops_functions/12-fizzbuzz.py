cd49480f2a6d06c5f603e52f880bfe10f82841eb
mugabe-rob <mugaberobert222004644@gmail.com>
2023-11-28 12:29:50 -0800

    A function that prints numbers from 1 to 100 separated by space

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,10 @@
+def fizzbuzz():
+    for no in range(1, 101):
+        if no % 3 == 0 and no % 5 == 0:
+            print("FizzBuzz", end=" ")
+        elif no % 3 == 0:
+            print("Fizz", end=" ")
+        elif no % 5 == 0:
+            print("Buzz", end=" ")
+        else:
+            print(no, end=" ")
