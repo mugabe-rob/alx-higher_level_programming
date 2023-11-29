cead73cd4e4c5f9237519701391bc5b4d81c8449
mugabe-rob <mugaberobert222004644@gmail.com>
2023-11-29 04:33:20 -0800

    A program to assign a random number to the variable each time it's executed

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,12 +2,13 @@
 import random
 number = random.randint(-10000, 10000)
 if number < 0:
-    last_digit = number % -10
-elif number >= 0:
-    last_digit = number % 10
-if last_digit > 5:
-    print(f"Last digit of {number} is {last_num} and is greater than 5")
-elif last_digit == 0:
-    print(f"Last digit of {number} is {last_num} and is 0")
+    last_digit = ((number * -1) % 10) * -1
 else:
-    print(f"Last digit of {number} is {last_num} and is less than 6 and not 0")
+    last_digit = number % 10
+print(f"Last digit of {number:d} is {last_digit:d} and is", end=" ")
+if last_digit > 5:
+    print("greater than 5")
+elif last_digit == 0:
+    print("0")
+else:
+    print("less than 6 and not 0")
