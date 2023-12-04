1cd4aaefc2219ac367076d637197a87234365c77
mugabe-rob <mugaberobert222004644@gmail.com>
2023-12-04 11:59:50 -0800

    A function that prints amatrix of integers

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-
 def print_matrix_integer(matrix=[[]]):
-    for counter in matrix:
-        for column in counter:
-            print("{:d}".format(column), end=" " if column != counter[-1] else "")
+    for i in matrix:
+        for j in i:
+            print("{:d}".format(j), end=" " if j != i[-1] else "")
         print()
