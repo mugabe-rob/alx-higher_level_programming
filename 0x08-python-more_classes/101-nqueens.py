76bfc106376a5b3b56a7137f8c5b14b86efe72b0
mugabe-rob <mugaberobert222004644@gmail.com>
2024-01-04 12:39:16 -0800

    N Queens

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,62 +1,41 @@
 #!/usr/bin/python3
-"""
-nqueens backtracking program to print the coordinates of n queens
-on an nxn grid such that they are all in non-attacking positions
-"""
+import sys
 
 
-from sys import argv
-
-if __name__ == "__main__":
-    a = []
-    if len(argv) != 2:
-        print("Usage: nqueens N")
-        exit(1)
-    if argv[1].isdigit() is False:
-        print("N must be a number")
-        exit(1)
-    n = int(argv[1])
-    if n < 4:
-        print("N must be at least 4")
-        exit(1)
-
-    # initialize the answer list
-    for i in range(n):
-        a.append([i, None])
-
-    def already_exists(y):
-        """check that a queen does not already exist in that y value"""
-        for x in range(n):
-            if y == a[x][1]:
-                return True
-        return False
-
-    def reject(x, y):
-        """determines whether or not to reject the solution"""
-        if (already_exists(y)):
-            return False
-        i = 0
-        while(i < x):
-            if abs(a[i][1] - y) == abs(i - x):
+def solve_n_queens(N):
+    def can_place(pos, oc_positions):
+        for i in range(len(oc_positions)):
+            if oc_positions[i] == pos or \
+                    oc_positions[i] - i == pos - len(oc_positions) or \
+                    oc_positions[i] + i == pos + len(oc_positions):
                 return False
-            i += 1
         return True
 
-    def clear_a(x):
-        """clears the answers from the point of failure on"""
-        for i in range(x, n):
-            a[i][1] = None
+    def place_queen(oc_positions, target_row, N):
+        if target_row == N:
+            result.append(oc_positions)
+            return
+        for column in range(N):
+            if can_place(column, oc_positions):
+                place_queen(oc_positions + [column], target_row + 1, N)
 
-    def nqueens(x):
-        """recursive backtracking function to find the solution"""
-        for y in range(n):
-            clear_a(x)
-            if reject(x, y):
-                a[x][1] = y
-                if (x == n - 1):  # accepts the solution
-                    print(a)
-                else:
-                    nqueens(x + 1)  # moves on to next x value to continue
+    result = []
+    place_queen([], 0, N)
+    return result
 
-    # start the recursive process at x = 0
-    nqueens(0)
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+    if not sys.argv[1].isdigit():
+        print("N must be a number")
+        sys.exit(1)
+    N = int(sys.argv[1])
+    if N < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+
+    solutions = solve_n_queens(N)
+    for sol in solutions:
+        print([[i, sol[i]] for i in range(N)])
