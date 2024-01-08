b90f85a0f9ac327cafd50ccd7672e51c2ddc4744
mugabe-rob <mugaberobert222004644@gmail.com>
2024-01-08 03:35:42 -0800

    a class BaseGeometry (based on 5-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Containing the BaseGeometry class
+"""
+
+class BaseGeometry:
+    """public attribute area class"""
+    def area(self):
+        """raising an exception when the the function is called"""
+        raise Exception("area() is not implemented")
