fc74d07df8158e473cc5685ee00e84da9c54943e
mugabe-rob <mugaberobert222004644@gmail.com>
2024-02-14 08:14:16 -0800

    Creating a table

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,2 +1,3 @@
--- creating a table
-CREATE TABLE IF EXISTS `id_not_null` (`id` INT	DEFAULT 1, `name` VARCHAR(256));
+-- creates the table id_not_null on your MySQL server
+-- creates a table
+CREATE TABLE IF NOT EXISTS id_not_null (id INT DEFAULT 1, name VARCHAR(256));
