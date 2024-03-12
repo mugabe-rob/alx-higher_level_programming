e7b303d068474fce61ca3a0aaff3ef8047c2f2c9
mugabe-rob <mugaberobert222004644@gmail.com>
2024-03-12 05:57:02 -0700

    Adding an instance method

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
-module.exports = class Rectangle{
-	constructor (w, h) {
-		if (w > 0 && h >0) { [this.width, this.height] = [w, h]; }
-	}
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) { [this.width, this.height] = [w, h]; }
+  }
 
-	print () {
-		for (let counter = 0; counter < this.height; counter++) console.log('X'.repeat(this.width));
-	}
+  print () {
+    for (let i = 0; i < this.height; i++) console.log('X'.repeat(this.width));
+  }
 };
