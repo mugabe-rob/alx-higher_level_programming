5c43784eb6ea3e8b4d6a2128da21ec0e06de7177
mugabe-rob <mugaberobert222004644@gmail.com>
2024-03-15 13:31:38 -0700

    A script to take the name of a state as an argument

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,21 +1,29 @@
 #!/usr/bin/python3
-"""
-5-filter_cities module
-"""
+""" This script selects states with names matching arguments """
+
+
+from sys import argv
 import MySQLdb
-import sys
 
 if __name__ == '__main__':
-    db = MySQLdb.connect(host='localhost', port=3306,
-                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
 
-    cur = db.cursor()
-    cur.execute("""SELECT cities.name FROM
-                cities INNER JOIN states ON states.id=cities.state_id
-                WHERE states.name=%s
-                ORDER BY cities.id ASC""", (sys.argv[4],))
-    rows = cur.fetchall()
-    new_list = list(row[0] for row in rows)
-    print(", ".join(new_list))
-    cur.close()
-    db.close()
+    db_user = argv[1]
+    db_password = argv[2]
+    db_name = argv[3]
+    user_state = argv[4]
+
+    database = MySQLdb.connect(host='localhost',
+                               port=3306,
+                               user=db_user,
+                               password=db_password,
+                               db=db_name)
+
+    cursor = database.cursor()
+
+    cursor.execute('SELECT cities.name FROM cities\
+                   JOIN states\
+                   ON cities.state_id = states.id\
+                   WHERE states.name = %s\
+                   ORDER BY states.id ASC', (user_state,))
+    result = []
+    print(', '.join([value[0] for value in cursor.fetchall()]))
