4eac284de9be91468728bddaadbdb491e27ddfd1
mugabe-rob <mugaberobert222004644@gmail.com>
2024-03-15 13:12:35 -0700

    A script to list all states with a name stating with N

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,25 +1,19 @@
 #!/usr/bin/python3
-""" This script lists all states with a name starting with N """
+"""
+1-filter_states module
+"""
+import MySQLdb
+import sys
 
 if __name__ == '__main__':
-    
-    from sys import argv
-    import MySQLdb
+    db = MySQLdb.connect(host='localhost', port=3306,
+                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
 
-    db_user = argv[1]
-    db_password = argv[2]
-    db_name = argv[3]
-
-    database = MySQLdb.connect(host='localhost',
-                               port=3306,
-                               user=db_user,
-                               password=db_password,
-                               db=db_name)
-
-    cursor = database.cursor()
-
-    cursor.execute('SELECT id, name FROM states ORDER BY states.id ASC')
-    
-    for row in cursor.fetchall():
-        if row[1][0] == 'N':
-            print(row)
+    cur = db.cursor()
+    cur.execute("""SELECT * FROM states WHERE name
+                LIKE BINARY 'N%' ORDER BY states.id ASC""")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
